3f72360e4b7673b8cc7629f94a7a7bf425fb69cb
Amen Jlili <amen@bluebyte.biz>
2022-03-29 14:08:10 -0700

    Migrated WordPressPCL to .net472

--- WordPressPCL.Tests.Selfhosted/Utility/ApiCredentials.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WordPressPCL.Tests.Selfhosted.Utility
-{
-    public class ApiCredentials
-    {
-        public static string WordPressUri = "http://localhost/wp-json/";
-        public static string Username = "wordpress";
-        public static string Password = "wordpress";
-    }
-}
